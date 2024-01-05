4d34d18670c15c51946ae62124d77ec6e78e1728
Timothevtl <115265913+Timothevtl@users.noreply.github.com>
2024-01-05 11:56:49 +0100

    Update app.py

--- Desktop/app_streamlit/app.py
+++ Desktop/app_streamlit/app.py
@@ -1,6 +1,7 @@
 import os
 import random
 import streamlit as st
+st.write("Python executable:", sys.executable)
 import nltk
 import requests
 import pickle
@@ -8,8 +9,8 @@ from io import BytesIO
 from nltk.corpus import sentiwordnet as swn
 from nltk import pos_tag, word_tokenize
 from nltk.sentiment import SentimentIntensityAnalyzer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.metrics.pairwise import linear_kernel
 
 # Download necessary NLTK resources
 nltk.download('punkt')
